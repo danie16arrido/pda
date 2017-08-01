f943743dc3f37e400f99a415d56d319320547d6e
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-08-01 18:16:02 +0100

    pass tests for subtract and multiply functions

--- js_calculator_start_point/tests/unit/calculator_spec.js
+++ js_calculator_start_point/tests/unit/calculator_spec.js
@@ -47,6 +47,86 @@ describe('calculator', function () {
     assert.equal(1, calculator.runningTotal)
   })
 
+  //Subtract///
+  it('it can substract 0 to 0', function () {
+    calculator.previousTotal = 0
+    calculator.subtract(0)
+    assert.equal(0, calculator.runningTotal)
+  })
+
+  it('it can subtract from negative number', function () {
+    calculator.previousTotal = -3
+    calculator.subtract(4)
+    assert.equal(-7, calculator.runningTotal)
+  })
+
+  it('it can handle double negation)', function () {
+    calculator.previousTotal = 3
+    calculator.subtract(-4)
+    assert.equal(7, calculator.runningTotal)
+  })
+
+  it('it can handle double negation up', function () {
+    calculator.previousTotal = -3
+    calculator.subtract(-4)
+    assert.equal(1, calculator.runningTotal)
+  })
+
+  it('it can add subtract from positive number', function () {
+    calculator.previousTotal = 7
+    calculator.subtract(4)
+    assert.equal(3, calculator.runningTotal)
+  })
+
+  it('it can handle subtract to a negative number', function () {
+    calculator.previousTotal = -3
+    calculator.subtract(4)
+    assert.equal(-7, calculator.runningTotal)
+  })
+  ///Multiply
+  it('it can multiply 2 positive numbers', function () {
+    calculator.previousTotal = 3
+    calculator.multiply(3)
+    assert.equal(9, calculator.runningTotal)
+  })
+
+  it('it can multiply 2 negative numbers', function () {
+    calculator.previousTotal = -3
+    calculator.multiply(-3)
+    assert.equal(9, calculator.runningTotal)
+  })
+
+  it('it can multiply a negative and positive number', function () {
+    calculator.previousTotal = -3
+    calculator.multiply(3)
+    assert.equal(-9, calculator.runningTotal)
+  })
+
+  it('it can multiply a negative and positive number v2', function () {
+    calculator.previousTotal = 3
+    calculator.multiply(-3)
+    assert.equal(-9, calculator.runningTotal)
+  })
+
+  it('it can handle positive multiply by 0', function () {
+    calculator.previousTotal = 3
+    calculator.multiply(0)
+    assert.equal(0, calculator.runningTotal)
+  })
+
+  it('it can handle negative multiply by 0', function () {
+    calculator.previousTotal = -3
+    calculator.multiply(0)
+    assert.equal(0, calculator.runningTotal)
+  })
+
+  it('it can handle 0 by 0', function () {
+    calculator.previousTotal = 0
+    calculator.multiply(0)
+    assert.equal(0, calculator.runningTotal)
+  })
+  ///Divide
+
 
 
 });
